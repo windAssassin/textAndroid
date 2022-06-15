f449f3fc0646aa66ef9a8c09bd683059cf5de348
zs <zhangshen1@cmcm.com>
2022-06-15 16:21:03 +0800

    删除无用代码

--- app/src/main/java/com/zs/androidapp/MainActivity.java
+++ app/src/main/java/com/zs/androidapp/MainActivity.java
@@ -13,16 +13,9 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private TextAdapter mTextAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-
-    public void text(){
-
     }
 }
